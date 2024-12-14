e0d79e8e058edde9ff6e3c7a5af2f9b537b0148d
Reddxforyou <aislakana@gmail.com>
2024-12-14 16:41:45 +0700

    fix patch

--- include/Response.h
+++ include/Response.h
@@ -1,1 +1,10 @@
+#ifndef RESPONSE_H
+#define RESPONSE_H	
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "HTTP_Server.h"
+#include "Routes.h"
+#define MAX_REQUEST_SIZE 4096
+
 char * render_static_file(char* fileName);
--- src/main.c
+++ src/main.c
@@ -2,11 +2,20 @@
 #include "Server.h"
 
 int main() {
-    // Menangani sinyal SIGCHLD
-    setup_sigchld_handler();
-    
-    // Mulai server pada port 8080
-    start_server(8080);
+    signal(SIGCHLD, handle_sigchld);
 
+
+    HTTP_Server http_server;
+    sem_init(&http_server.semaphore, 0, 20);  // Maksimum 20 proses anak bersamaan
+    init_server(&http_server, 6969);
+
+    struct Route *route = initRoute("/", "index.html");
+    addRoute(route, "/about", "about.html");
+    inorder(route);
+
+    process_client_connection(http_server.socket, route, &http_server);
+
+    sem_destroy(&http_server.semaphore);
+    close(http_server.socket);
     return 0;
 }
